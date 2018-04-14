a74b253f64fc68fdc058323138ab3d93134f7476
Laura Contreras <monicacora2016@gmail.com>
2018-04-14 12:42:48 -0500

    Monica Contreras
    Actividad 4

---
+++ MonicaContreras-AirCombt/Assets/Scripts/MisilFalling.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MisilFalling : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision collision){
+		// Eliminamos el piso
+		Destroy(collision.collider.gameObject);
+
+	}
+}
